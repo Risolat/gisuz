323936b8b988ee0448e8af93bef7a69eeecc462f
Risolat Utkurova <risolatutkurova@Risolats-MacBook-Air.local>
2023-09-06 16:44:05 +0500

    fix

--- pages/interactive_service/appeal.js
+++ pages/interactive_service/appeal.js
@@ -427,7 +427,7 @@ const page = () => {
                             {t("form.message-text")}
                           </p>
                           <label className="flex flex-col mr-0 xl:mr-[20px]">
-                            <input
+                            <textarea
                               type="text"
                               name="text"
                               placeholder=""
